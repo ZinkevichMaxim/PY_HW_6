ccf646bfcb27419219c75e74b3b62b9881e5600d
ZINKEVICH Maksim <drmvz@Imail.ru>
2023-08-01 17:45:38 +0300

    fin

--- Task_32.py
+++ Task_32.py
@@ -5,14 +5,18 @@ import random
 
 min = int(input('Введите минимальное значение элемента \n'))
 max = int(input('Введите максимальное значение элемент \n'))
-n = int(input('Введите длину списка \n'))
+# n = int(input('Введите длину списка \n'))
 
-# list = []
+list = [1, 3, 5, 6, 17, 9, 11, 13, 10, 6]
 
-list = [i for i in range(n)]
+# list = [i for i in range(n)]
 list_res = []
 
 # print(list)
 
-for i in range(n):
-    if 
+for i in range(len(list)):
+    if list[i]>min and list[i]<max:
+        list_res.append(i)
+    
+print(list)
+print(list_res)
